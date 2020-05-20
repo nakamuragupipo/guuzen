69c994d27c11099f8ccbefa2440d91fd17a1153b
nakamuragupipo <nakamuragupipo@users.noreply.github.com>
2020-05-20 16:44:32 +0900

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # railsチュートリアルで追記
+  config.force_ssl = true
 end
