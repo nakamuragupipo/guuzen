a6e185e64d7b182e498a01bd95d0434ca9bb9083
nakamuragupipo <nakamuragupipo@users.noreply.github.com>
2020-05-29 20:03:09 +0900

    追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true　　　　#trueに上書き
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://fast-falls-861578.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
